d82a63345dc2f75fe2c05f7e444ec4ce1c11cd9b
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-08-05 17:05:32 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketPairs = {}
+    let stack = []
+
+    bracketsConfig.forEach(pair => {
+        let [open, close] = pair
+        bracketPairs[open] = close
+    });
+
+    let open_brackets = Object.keys(bracketPairs)
+
+    for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i]
+
+        if (open_brackets.includes(currentSymbol)) {
+            if (bracketPairs[currentSymbol] === currentSymbol) {
+                if (stack[stack.length - 1] === currentSymbol) {
+                    stack.pop()
+                } else {
+                    stack.push(currentSymbol)
+                }
+            } else {
+                stack.push(currentSymbol)
+            }
+        } else {
+            if (stack.length === 0) {
+                return false
+            }
+
+            let topElement = stack[stack.length - 1]
+
+            if (bracketPairs[topElement] === currentSymbol) {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
